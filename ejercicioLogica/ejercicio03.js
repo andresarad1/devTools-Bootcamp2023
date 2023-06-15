cb366e03d39583940218342260e89793728c4ef3
andresarad1 <andresarad@misena.edu.co>
2023-06-15 00:26:44 -0500

    definicion del reto y sus ejercicios

---
+++ ejercicioLogica/ejercicio03.js
@@ -0,0 +1,17 @@
+/*
+ Contador de existencias
+Dado un array de nombres, tu objetivo es crear una función que cuente cuántas veces se repite cada nombre y los represente con asteriscos (*).
+Debes implementar la función countNameRepetitions(names), que tomará como parámetro un array de nombres y devolverá un objeto con los nombres y su
+representación de asteriscos correspondiente.
+Ejemplo:
+const nombres = ['Juan', 'María', 'Pedro', 'Juan', 'María', 'María'];
+// Llamada a la función
+const resultado = countNameRepetitions(nombres);
+// Resultado esperado
+//   Juan: '**',
+//   María: '***',
+//   Pedro: '*'
+En el ejemplo anterior, se tiene un array de nombres donde 'Juan' se repite dos veces, 'María' se repite tres veces y 'Pedro' se repite una vez.
+La función countNameRepetitions debe imprimir los nombres y su representación de asteriscos correspondiente.
+Recuerda que cada vez que un nombre se repite, se debe agregar un asterisco adicional al valor asociado al nombre en el objeto de resultado
+*/
---
+++ ejercicioLogica/ejercicio04.js
@@ -0,0 +1,32 @@
+/*
+FizzBuzz
+Tu tarea es escribir un programa que recorra los números del 1 al n. Para cada número:
+Si el número es divisible por 3, debes imprimir "Fizz".
+Si el número es divisible por 5, debes imprimir "Buzz".
+Si el número es divisible tanto por 3 como por 5, debes imprimir "FizzBuzz".
+Si el número no es divisible ni por 3 ni por 5, debes imprimir el número tal cual.
+Para lograrlo debes implementar una función llamada fizzBuzz(n), que tome como parámetro un número entero n y ejecute el juego FizzBuzz.
+Ejemplo:
+// Llamada a la función
+fizzBuzz(15);
+// Resultado esperado
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+En el ejemplo anterior, la función fizzBuzz se ejecuta con el número 15. El programa recorre los números del 1 al 15 y aplica las reglas del juego FizzBuzz. Los números que
+son divisibles por 3 se reemplazan por "Fizz", los números divisibles por 5 se reemplazan por "Buzz" y los números divisibles tanto por 3 como por 5 se reemplazan por
+"FizzBuzz". El resultado se imprime en la consola.
+Tu objetivo es implementar la función fizzBuzz de manera que cumpla con las reglas del juego y produzca el resultado esperado
+*/

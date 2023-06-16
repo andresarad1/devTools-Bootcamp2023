7e131230c80343c22e56f95fb4104ac285a337ef
andresarad1 <andresarad@misena.edu.co>
2023-06-15 20:20:37 -0500

    Resolucion segundo ejercicio

--- ejercicioLogica/ejercicio02.js
+++ ejercicioLogica/ejercicio02.js
@@ -4,11 +4,19 @@ Crear un número de teléfono
 Escribe una función que acepte un array con 10 números enteros positivos (entre 0 y 9) y devuelva una cadena de texto con esos números en 
 forma de número de teléfono.
 Ejemplo:
-createPhoneNumber([5, 5, 5, 1, 3, 4, 7, 8, 9, 0]) => devuelve "(555) 134-
-7890"
- createPhoneNumber([3, 0, 5, 1, 2, 7, 7, 2, 4, 9]) => devuelve "(305) 127-
-7249"
+createPhoneNumber([5, 5, 5, 1, 3, 4, 7, 8, 9, 0]) => devuelve "(555) 134-7890"
+createPhoneNumber([3, 0, 5, 1, 2, 7, 7, 2, 4, 9]) => devuelve "(305) 127-7249"
 
 El formato del número de teléfono devuelto debe ser correcto para completar este desafío. ¡No olvides el espacio después 
 del paréntesis de cierre!
 */
+function createPhoneNumber(array) {
+    array = array.join("").toString()
+    let format = "(" + array.slice(0,3) + ") " + array.slice(3, 6) + "-" + array.slice(6, array.length)
+    console.log(format);
+    return format
+}
+
+let format = createPhoneNumber([5, 5, 5, 1, 3, 4, 7, 8, 9, 0])
+
+format = createPhoneNumber([3, 0, 5, 1, 2, 7, 7, 2, 4, 9])
